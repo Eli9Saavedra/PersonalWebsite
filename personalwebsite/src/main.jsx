4ecb6f5b7499ad72eb826c0e34783041428bcf9b
elisaa98 <elisaa@epcad.org>
2026-05-07 07:13:48 -0600

    Added Speed Insights

--- personalwebsite/src/main.jsx
+++ personalwebsite/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
-import {Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
+import {SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         <ThemeProvider>
                 <App /> {/* This now becomes {children} */}
                 <Analytics />
+                <SpeedInsights />
                 </ThemeProvider>
        </BrowserRouter>
   </StrictMode>,
